309b5c797c7c1f9e19161dd760fd673d34902c89
maria <mari.borozdenko@gmail.com>
2017-07-10 22:06:54 +0300

    added DefaultController - redirect from unmapped urls to main page

---
+++ web/src/main/java/ru/kraftlab/web/web/controller/DefaultController.java
@@ -0,0 +1,14 @@
+package ru.kraftlab.web.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class DefaultController {
+    @RequestMapping(value = "/*", method = RequestMethod.GET)
+    public ModelAndView build() {
+        return new ModelAndView("redirect:/main");
+    }
+}
