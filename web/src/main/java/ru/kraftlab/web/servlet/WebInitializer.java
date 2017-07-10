3582912bf8ffc9d7c4d8d1e51eff281b8255e9d5
maria <mari.borozdenko@gmail.com>
2017-07-10 16:01:18 +0300

    set default spring profile as "standalone"

--- web/src/main/java/ru/kraftlab/web/servlet/WebInitializer.java
+++ web/src/main/java/ru/kraftlab/web/servlet/WebInitializer.java
@@ -1,12 +1,21 @@
 package ru.kraftlab.web.servlet;
 
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 import ru.kraftlab.app.config.DBConfig;
 import ru.kraftlab.app.config.StandaloneDBConfig;
 import ru.kraftlab.web.config.SpringWebConfig;
 
-public class WebInitializer extends
-        AbstractAnnotationConfigDispatcherServletInitializer {
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    private static final String EMBEDDED_DB_PROFILE = "standalone";
+
+    @Override
+    protected WebApplicationContext createRootApplicationContext() {
+        WebApplicationContext rootContext = super.createRootApplicationContext();
+        ((ConfigurableEnvironment) rootContext.getEnvironment()).setDefaultProfiles(EMBEDDED_DB_PROFILE);
+        return rootContext;
+    }
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
